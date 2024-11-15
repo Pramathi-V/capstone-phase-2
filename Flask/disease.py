1a7a2a6af66ceeac85b45f7daa9896c8d723b416
saimani1112 <138869063+saimani1112@users.noreply.github.com>
2024-11-15 17:21:48 +0530

    Add files via upload

---
+++ Flask/disease.py
@@ -0,0 +1,306 @@
+import os
+import random
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import random_split
+from torch.utils.data import DataLoader, Dataset, Subset
+from torch.utils.data import random_split, SubsetRandomSampler
+from torchvision import datasets, transforms, models 
+from torchvision.datasets import ImageFolder
+from torchvision.transforms import ToTensor
+from torchvision.utils import make_grid
+from pytorch_lightning import LightningModule
+from pytorch_lightning import Trainer
+import pytorch_lightning as pl
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from PIL import Image
+
+
+transform=transforms.Compose([
+        transforms.RandomRotation(10),      # rotate +/- 10 degrees
+        transforms.RandomHorizontalFlip(),  # reverse 50% of images
+        transforms.Resize(224),             # resize shortest side to 224 pixels
+        transforms.CenterCrop(224),         # crop longest side to 224 pixels at center
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406],
+                             [0.229, 0.224, 0.225])
+])
+
+class CustomDataset(torch.utils.data.Dataset):
+    def __init__(self, path_label, transform=None):
+        self.path_label = path_label
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.path_label)
+
+    def __getitem__(self, idx):
+        path, label = self.path_label[idx]
+        img = Image.open(path).convert('RGB')
+
+        if self.transform is not None:
+            img = self.transform(img)
+        return img, label
+
+
+class ImageDataset(pl.LightningDataModule):
+    def __init__(self, path_label, batch_size=32):
+        super().__init__()
+        self.path_label = path_label
+        self.batch_size = batch_size
+        self.transform = transforms.Compose([
+            transforms.Resize(224),             # resize shortest side to 224 pixels
+            transforms.CenterCrop(224),         # crop longest side to 224 pixels at center            
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406],
+                                 [0.229, 0.224, 0.225])
+        ])
+
+
+    def setup(self, stage=None):
+        if stage=='Test':
+            dataset = CustomDataset(self.path_label, self.transform)
+            dataset_size = len(dataset)
+            print(dataset_size)
+            self.test_dataset = torch.utils.data.Subset(dataset, range(dataset_size))    
+
+        else:
+            dataset = CustomDataset(self.path_label, self.transform)
+            dataset_size = len(dataset)
+            train_size = int(0.8 * dataset_size) 
+            val_size = dataset_size - train_size
+            print(train_size,val_size)
+
+            self.train_dataset = torch.utils.data.Subset(dataset, range(train_size))
+            self.val_dataset = torch.utils.data.Subset(dataset, range(train_size, dataset_size))
+
+    def __len__(self):
+        if self.train_dataset is not None:
+            return len(self.train_dataset)
+
+        elif self.val_dataset is not None:
+            return len(self.val_dataset)
+
+        else:
+            return 0        
+
+    def __getitem__(self, index):
+        if self.train_dataset is not None:
+            return self.train_dataset[index]
+
+        elif self.test_dataset is not None:
+            return self.test_dataset[index]
+
+        else:
+            raise IndexError("Index out of range. The dataset is empty.")
+
+    def train_dataset(self):
+        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
+
+    def val_dataset(self):
+        return DataLoader(self.val_dataset, batch_size=self.batch_size)
+
+    def test_dataset(self):
+        return DataLoader(self.test_dataset, batch_size=self.batch_size)
+
+
+class DataModule(pl.LightningDataModule):
+    def __init__(self, transform=transform, batch_size=16):
+        super().__init__()
+        dir0='data/Rice_Leaf_Diease/Rice_Leaf_Diease/train'
+        dir2='data/Rice_Leaf_Diease/Rice_Leaf_Diease/test'
+        self.root_dir = dir0
+        self.test_dir = dir2
+        self.transform = transform
+        self.batch_size = batch_size
+
+    def setup(self, stage=None):
+        if stage == 'Test':
+            # Load the test set for the 'Test' stage
+            test_set = datasets.ImageFolder(root=self.test_dir, transform=self.transform)
+            self.test_dataset = test_set  # Save the dataset for later use
+
+        else:
+            dataset = datasets.ImageFolder(root=self.root_dir, transform=self.transform)
+            n_data = len(dataset)
+            n_train = int(0.8 * n_data)
+            n_val = n_data - n_train
+            train_dataset, val_dataset = random_split(dataset, [n_train, n_val])
+            self.train_dataset = train_dataset
+            self.val_dataset = val_dataset
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_dataset, batch_size=self.batch_size)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_dataset, batch_size=self.batch_size)
+
+
+class ConvolutionalNetwork(LightningModule):
+    def __init__(self):
+        class_names = ['bacterial_leaf_blight','brown_spot','healthy','leaf_blast','leaf_scald','narrow_brown_spot','Neck_blast','Rice Hispa','Sheath Blight','Tungro']
+        super(ConvolutionalNetwork, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 3, 1)
+        self.conv2 = nn.Conv2d(6, 16, 3, 1)
+        self.fc1 = nn.Linear(16 * 54 * 54, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 20)
+        self.fc4 = nn.Linear(20, len(class_names))
+
+    def forward(self, X):
+        X = F.relu(self.conv1(X))
+        X = F.max_pool2d(X, 2, 2)
+        X = F.relu(self.conv2(X))
+        X = F.max_pool2d(X, 2, 2)
+        X = X.view(-1, 16 * 54 * 54)
+        X = F.relu(self.fc1(X))
+        X = F.relu(self.fc2(X))
+        X = F.relu(self.fc3(X))
+        X = self.fc4(X)
+        return F.log_softmax(X, dim=1)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+        return optimizer
+
+
+    def training_step(self, train_batch, batch_idx):
+        X, y = train_batch
+        y_hat = self(X)
+        loss = F.cross_entropy(y_hat, y)
+        pred = y_hat.argmax(dim=1, keepdim=True)
+        acc = pred.eq(y.view_as(pred)).sum().item() / y.shape[0]
+        self.log("train_loss", loss)
+        self.log("train_acc", acc)
+        return loss
+
+    def validation_step(self, val_batch, batch_idx):
+        X, y = val_batch
+        y_hat = self(X)
+        loss = F.cross_entropy(y_hat, y)
+        pred = y_hat.argmax(dim=1, keepdim=True)
+        acc = pred.eq(y.view_as(pred)).sum().item() / y.shape[0]
+        self.log("val_loss", loss)
+        self.log("val_acc", acc)
+
+    def test_step(self, test_batch, batch_idx):
+        X, y = test_batch
+        y_hat = self(X)
+        loss = F.cross_entropy(y_hat, y)
+        pred = y_hat.argmax(dim=1, keepdim=True)
+        acc = pred.eq(y.view_as(pred)).sum().item() / y.shape[0]
+        self.log("test_loss", loss)
+        self.log("test_acc", acc)
+
+
+
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ConvolutionalNetwork(nn.Module):
+    def __init__(self):
+        super(ConvolutionalNetwork, self).__init__()
+        
+        # Define convolutional layers
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=6, kernel_size=3)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=3)
+        
+        # Define fully connected layers
+        # Ensure the input to fc1 matches 46656
+        self.fc1 = nn.Linear(in_features=46656, out_features=120)
+        self.fc2 = nn.Linear(in_features=120, out_features=84)
+        self.fc3 = nn.Linear(in_features=84, out_features=20)
+        self.fc4 = nn.Linear(in_features=20, out_features=10)
+
+    def forward(self, x):
+        # Apply convolutional layers with pooling and activation
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2)
+        
+        # Flatten the output for fully connected layers
+        x = x.view(x.size(0), -1)  # Make sure this results in a size of 46656
+        
+        # Apply fully connected layers
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
+        
+        return x
+
+
+
+def load_model(model_path):
+    loaded_model = ConvolutionalNetwork()
+    loaded_model.load_state_dict(torch.load(model_path))
+    loaded_model.eval()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    loaded_model.to(device)
+    return loaded_model, device
+
+
+# Function to predict the class of an input image
+
+def predict_image(image_path, model_path):
+    #model_path = 'Flask\convolutional_network.pth'
+    #state_dict = torch.load(model_path, map_location='cpu')
+    loaded_model = ConvolutionalNetwork()
+    loaded_model.load_state_dict(torch.load(model_path))
+    loaded_model.eval()
+    print(f"Model loaded from {model_path}")
+    class_names = ['bacterial_leaf_blight','brown_spot','healthy','leaf_blast','leaf_scald','narrow_brown_spot','Neck_blast','Rice Hispa','Sheath Blight','Tungro']
+    model, device = load_model(model_path)
+    transform = transforms.Compose([
+        transforms.Resize(224),             # resize shortest side to 224 pixels
+        transforms.CenterCrop(224),         # crop longest side to 224 pixels at center
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+    image = Image.open(image_path).convert('RGB')
+    image = transform(image)
+    image = image.unsqueeze(0).to(device)
+
+    with torch.no_grad():
+        output = model(image)
+        prediction = torch.argmax(output, dim=1).item()  # Get the predicted class index
+    predicted_class = class_names[prediction]
+    return predicted_class
+
+
+
+
+# Example usage
+
+# model_path = r'Flask\\convolutional_network.pth'  # Path to the saved model
+# input_image_path = r'D:\\PES_Classes\\Sem7\\SMC\\capstone-phase-2\\Flask\\Screenshot 2024-11-07 215847.png'  # Path to the input image
+
+# Load the model
+#model, device = load_model(model_path)
+#class_names = ['bacterial_leaf_blight','brown_spot','healthy','leaf_blast','leaf_scald','narrow_brown_spot','Neck_blast','Rice Hispa','Sheath Blight','Tungro']
+
+# Predict the class of the input image
+# predicted_class = predict_image(input_image_path)
+# print(f"The predicted class for the input image is: {predicted_class}")
+
+
+import pandas as pd
+
+predicted_class = "neck_blast"
+disease_name = predicted_class.split("\\")[-1]  # Extract 'rice_hispa'
+
+
+csv_file_path = r'D:\capstone-phase-2\Flask\pest_disease_table.csv'
+
