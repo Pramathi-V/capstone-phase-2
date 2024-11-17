7147ea80c825d8c9123df23d66b7992df92208ff
PramathiV <pravum2003@gmail.com>
2024-11-17 23:54:11 +0530

    background

--- src/App.js
+++ src/App.js
@@ -8,50 +8,59 @@ import Irrigation from "./Irrigation";
 import CropGrowthCycle from "./CropGrowthCycle";
 import AnomalyDetection from "./AnomalyDetection";
 import CropData from "./CropData";
+import backgroundImage from "./paddy-fields-1024x636.jpg"; // Import the image
 
 function App() {
   return (
     <DataProvider>
-      {" "}
-      {/* Wrap everything in DataProvider */}
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/crop-recommendation">Crop Recommendation</Link>
-              </li>
-              <li>
-                <Link to="/irrigation">Irrigation</Link>
-              </li>
-              <li>
-                <Link to="/crop-growth-cycle">Crop Growth Cycle</Link>
-              </li>
-              <li>
-                <Link to="/anomaly-detection">Anomaly Detection</Link>
-              </li>
-              <li>
-                <Link to="/crop-data">Crop Yield</Link>
-              </li>
-            </ul>
-          </nav>
+      <div
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          minHeight: "100vh",
+        }}
+      >
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/crop-recommendation">Crop Recommendation</Link>
+                </li>
+                <li>
+                  <Link to="/irrigation">Irrigation</Link>
+                </li>
+                <li>
+                  <Link to="/crop-growth-cycle">Crop Growth Cycle</Link>
+                </li>
+                <li>
+                  <Link to="/anomaly-detection">Anomaly Detection</Link>
+                </li>
+                <li>
+                  <Link to="/crop-data">Crop Yield</Link>
+                </li>
+              </ul>
+            </nav>
 
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route
-              path="/crop-recommendation"
-              element={<CropRecommendation />}
-            />
-            <Route path="/irrigation" element={<Irrigation />} />
-            <Route path="/crop-growth-cycle" element={<CropGrowthCycle />} />
-            <Route path="/anomaly-detection" element={<AnomalyDetection />} />
-            <Route path="/crop-data" element={<CropData />} />
-          </Routes>
-        </div>
-      </Router>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route
+                path="/crop-recommendation"
+                element={<CropRecommendation />}
+              />
+              <Route path="/irrigation" element={<Irrigation />} />
+              <Route path="/crop-growth-cycle" element={<CropGrowthCycle />} />
+              <Route path="/anomaly-detection" element={<AnomalyDetection />} />
+              <Route path="/crop-data" element={<CropData />} />
+            </Routes>
+          </div>
+        </Router>
+      </div>
     </DataProvider>
   );
 }
